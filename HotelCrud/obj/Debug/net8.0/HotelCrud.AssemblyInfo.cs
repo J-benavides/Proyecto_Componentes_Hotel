a4b0d1bd74f4f01d2ac2fbaa1078196858e183af
J-benavides <joshbenagu12@gmail.com>
2025-07-23 20:46:55 -0600

    ,

--- HotelCrud/obj/Debug/net8.0/HotelCrud.AssemblyInfo.cs
+++ HotelCrud/obj/Debug/net8.0/HotelCrud.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelCrud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bb71d4d37f94b656e20919e0accfe4ddcfd4d33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a69aba56b616153ebaa120aeaf6bbe2555cbfcb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelCrud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelCrud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
